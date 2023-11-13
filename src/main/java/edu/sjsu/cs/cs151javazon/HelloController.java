07e9876236b0f88c76d183d8e0af1c1340a33aea
Arctic3 <86848773+jack-200@users.noreply.github.com>
2023-11-12 18:08:19 -0800

    initial commit

--- src/main/java/edu/sjsu/cs/cs151javazon/HelloController.java
+++ src/main/java/edu/sjsu/cs/cs151javazon/HelloController.java
@@ -1,13 +1,21 @@
 package edu.sjsu.cs.cs151javazon;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onHelloButtonClick(ActionEvent event) {
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        MainProductPageController mainProductPageController = new MainProductPageController();
+        Parent root = mainProductPageController.getRoot(currentStage);
+        Scene currentScene = currentStage.getScene();
+        double adjustedWidth = currentStage.getWidth() - (currentStage.getWidth() - currentScene.getWidth());
+        double adjustedHeight = currentStage.getHeight() - (currentStage.getHeight() - currentScene.getHeight());
+        currentStage.setScene(new Scene(root, adjustedWidth + 1e-4, adjustedHeight));
     }
 }
