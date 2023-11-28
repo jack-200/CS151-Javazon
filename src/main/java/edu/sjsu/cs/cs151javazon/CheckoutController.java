c98bce8df12cc2e887241e0310b0be7528a630c0
jack-200 <86848773+jack-200@users.noreply.github.com>
2023-11-27 23:51:43 -0800

    formatting

--- src/main/java/edu/sjsu/cs/cs151javazon/CheckoutController.java
+++ src/main/java/edu/sjsu/cs/cs151javazon/CheckoutController.java
@@ -6,16 +6,16 @@ import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class CheckoutController {
-
     @FXML
     private TableView<Product> cartTable;
     @FXML
@@ -32,24 +32,29 @@ public class CheckoutController {
     private Button checkoutButton;
     @FXML
     private Label totalPriceLabel;
-
     @FXML
     public void initialize() {
         // Set up the table columns
         productNameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
-        quantityCol.setCellValueFactory(new PropertyValueFactory<>("quantity")); // Adjust if your Product class has a quantity field
+        quantityCol.setCellValueFactory(
+                new PropertyValueFactory<>("quantity")); // Adjust if your Product class has a quantity field
         priceCol.setCellValueFactory(new PropertyValueFactory<>("price"));
-
         // Load cart items
         loadCartItems();
-
         // Set up button actions
         //clearCartButton.setOnAction(event -> ShoppingCart.getInstance().clearCart());
         //checkoutButton.setOnAction(event -> performCheckout());
     }
-
+    private void loadCartItems() {
+        cartTable.getItems().setAll(ShoppingCart.getInstance().getProducts());
+        updateTotalPrice();
+    }
+    private void updateTotalPrice() {
+        double totalPrice = ShoppingCart.getInstance().getTotalPrice();
+        totalPriceLabel.setText(String.format("Total Price: $%.2f", totalPrice));
+    }
     @FXML
-    protected void onBackButtonClick(ActionEvent event){
+    protected void onBackButtonClick(ActionEvent event) {
         Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         MainProductPageController mainProductPageController = new MainProductPageController();
         Parent root = mainProductPageController.getRoot(currentStage);
@@ -58,33 +63,13 @@ public class CheckoutController {
         double adjustedHeight = currentStage.getHeight() - (currentStage.getHeight() - currentScene.getHeight());
         currentStage.setScene(new Scene(root, adjustedWidth, adjustedHeight));
     }
-
     private Button createButton(String label, Runnable action) {
         Button button = new Button(label);
         button.setOnAction(e -> action.run());
         return button;
     }
-
-    private void loadCartItems() {
-        cartTable.getItems().setAll(ShoppingCart.getInstance().getProducts());
-        updateTotalPrice();
-    }
-
-    private void updateTotalPrice() {
-        double totalPrice = ShoppingCart.getInstance().getTotalPrice();
-        totalPriceLabel.setText(String.format("Total Price: $%.2f", totalPrice));
-    }
-
     private void removeProductFromCart(Product product) {
         ShoppingCart.getInstance().removeProduct(product);
-        loadCartItems();
-    }
-
-
-    @FXML
-    //Clear the cart when the button is pressed
-    private void clearCart() {
-        ShoppingCart.getInstance().clearCart();
         loadCartItems();
     }
     @FXML
@@ -93,29 +78,30 @@ public class CheckoutController {
         clearCart();
         showCheckoutSuccessPopup();
     }
-
+    @FXML
+    //Clear the cart when the button is pressed
+    private void clearCart() {
+        ShoppingCart.getInstance().clearCart();
+        loadCartItems();
+    }
     private void showCheckoutSuccessPopup() {
         // Create a new stage (window)
         Stage popupStage = new Stage();
         popupStage.initModality(Modality.APPLICATION_MODAL); // Make it modal
         popupStage.setTitle("Checkout Success");
-
         // Create a label for the message
-        Label messageLabel = new Label("Successfully checked out. Total price: $" + String.format("%.2f", ShoppingCart.getInstance().getTotalPrice()));
-
+        Label messageLabel = new Label("Successfully checked out. Total price: $" +
+                                       String.format("%.2f", ShoppingCart.getInstance().getTotalPrice()));
         // Create a layout and add the label to it
         VBox layout = new VBox(10);
         layout.getChildren().add(messageLabel);
         layout.setAlignment(Pos.CENTER);
-
         // Create a scene with the layout and set it on the stage
         Scene scene = new Scene(layout, 300, 100);
         popupStage.setScene(scene);
-
         // Show the popup stage
         popupStage.show();
     }
-
 }
 
 
