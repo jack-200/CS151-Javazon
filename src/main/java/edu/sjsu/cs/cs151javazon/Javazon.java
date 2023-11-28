69550a1e5e50488bee52483bf5120c7c140e6d29
jack-200 <86848773+jack-200@users.noreply.github.com>
2023-11-28 00:59:02 -0800

    add popup fading message

--- src/main/java/edu/sjsu/cs/cs151javazon/Javazon.java
+++ src/main/java/edu/sjsu/cs/cs151javazon/Javazon.java
@@ -1,9 +1,15 @@
 package edu.sjsu.cs.cs151javazon;
 
+import javafx.animation.FadeTransition;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
@@ -22,14 +28,33 @@ public class Javazon extends Application {
     }
     public static void switchScene(String sceneName) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Javazon.class.getResource(sceneName));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 700, 500);
         Javazon.setScene(scene);
+    }
+    public static void showFadingPopup(ActionEvent actionEvent, String message) {
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Stage popupStage = new Stage();
+        popupStage.initOwner(currentStage);
+        popupStage.initStyle(javafx.stage.StageStyle.UNDECORATED);
+        popupStage.setAlwaysOnTop(true);
+        StackPane popupRoot = new StackPane();
+        popupRoot.setStyle("-fx-background-color: rgba(255, 255, 255, 0.7); -fx-padding: 10px;");
+        popupRoot.getChildren().add(new Label(message));
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(3), popupRoot);
+        fadeTransition.setFromValue(1.0);
+        fadeTransition.setToValue(0.0);
+        fadeTransition.setOnFinished(event -> popupStage.close());
+        popupStage.setScene(new Scene(popupRoot));
+        popupStage.setOnShown(event -> fadeTransition.play());
+        popupStage.setX(currentStage.getX() + (currentStage.getWidth() / 2) - (message.length() * 3));
+        popupStage.setY(currentStage.getY() + currentStage.getHeight() * 0.8);
+        popupStage.show();
     }
     @Override
     public void start(Stage stage) throws IOException {
         Javazon = stage;
         FXMLLoader fxmlLoader = new FXMLLoader(Javazon.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 650, 500);
+        Scene scene = new Scene(fxmlLoader.load(), 700, 500);
         stage.setTitle("Javazon");
         stage.setScene(scene);
         stage.show();
